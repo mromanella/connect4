21d9d25f81fd4571f03fabfa789a73bf08d42e8b
mromanella <mroms21@gmail.com>
2021-11-15 11:03:48 -0500

    Begin Phaser Rework

--- src/animator/objects/index.ts
+++
@@ -1,5 +0,0 @@
-import { BoundingBox, GameObject, Point } from "./game-objects";
-import * as shapes from "./shapes";
-import * as text from "./text";
-
-export  { BoundingBox, GameObject, Point, shapes, text };
